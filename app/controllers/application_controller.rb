342f2f3e3e0ae2a06bf1dbaeeb49aa2f4bd92c27
Aleeya Dasgupta <aleeya@gmail.com>
2018-04-05 00:07:37 +0530

    edit application controller action hello to sorry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-  	render html: "hello world"
+  def sorry
+  	render html: "i shouldn't have said that"
   end
-  
+
 end
